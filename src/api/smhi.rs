7adc4b8de8d5509891411ece019c463f099b31fd
Ulf Sjödin <sjodin.ulf@gmail.com>
2025-04-06 09:29:35 +0200

    Har fungerande nedladdning av json, lägger till modul för att hantera json.

--- src/api/smhi.rs
+++ src/api/smhi.rs
@@ -1,11 +1,41 @@
 use reqwest::Error;
 
+pub enum Parametrar {
+    Temperatur,
+    Luftryck,
+    Molnmangd,
+}
 
-pub async fn get_raw_json() -> Result<String, Error> {
-    println!("Hej från api/smhi.test");
-    let url = "https://opendata-download-metobs.smhi.se/api/version/latest/parameter/29/station/72420/period/latest-day/data.json";
+impl Parametrar {
+    pub fn to_id(&self) -> u32 {
+        match self {
+            Parametrar::Temperatur => 1,
+            Parametrar::Luftryck => 9,
+            Parametrar::Molnmangd => 29,
+        }
+    }
+}
+
+pub fn build_url(parametrar: Parametrar, station: u32) -> String {
+    format!(
+        "https://opendata-download-metobs.smhi.se/api/version/latest/parameter/{}/station/{}/period/latest-day/data.json",
+        parametrar.to_id(),
+        station
+    )
+}
+
+pub async fn fetch_json(url: &str) -> Result<String, Error> {
     let respons = reqwest::get(url).await?.text().await?;
     Ok(respons)
-
 }
+
+// pub async fn get_raw_json() -> Result<(String, String), Error> {
+//     println!("Hej från api/smhi.test");
+//     let molnmangd = "https://opendata-download-metobs.smhi.se/api/version/latest/parameter/29/station/72420/period/latest-day/data.json";
+//     let respons = reqwest::get(molnmangd).await?.text().await?;
+//     let lufttryck = "https://opendata-download-metobs.smhi.se/api/version/latest/parameter/9/station/72420/period/latest-day/data.json";
+//     let respons2 = reqwest::get(lufttryck).await?.text().await?;
+//     Ok((respons, respons2))
+
+// }
 
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,25 @@
 mod api;
-use api::smhi::{self, get_raw_json};
+// use std::os::linux::raw::stat;
+
+// use api::smhi::get_raw_json;
+use api::smhi::{fetch_json, build_url, Parametrar};
 
 #[tokio::main]
 async fn main() {
-    match get_raw_json().await {
-        Ok(json) => println!("Svar från SMHI: {}", json),
-        Err(e) => eprintln!("Inget svar fån SMHI {}", e),
-    }
+    let station = 72420;
+
+    let url_temp = build_url(Parametrar::Temperatur, station);
+    let url_tryck = build_url(Parametrar::Luftryck, station);
+
+    let jsontemperatur = fetch_json(&url_temp).await.unwrap();
+    let jsontryck = fetch_json(&url_tryck).await.unwrap();
+
+    println!("hela json med temperaturer: {}", jsontemperatur);
+    println!("--------------------------------------------------------");
+    println!("hela json med lufttryck: {}", jsontryck);
+
+    // match get_raw_json().await {
+    //     Ok(json) => println!("Svar från SMHI: {:?}", json),
+    //     Err(e) => eprintln!("Inget svar fån SMHI {}", e),
+    // }
 }
