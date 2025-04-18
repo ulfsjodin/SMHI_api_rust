dd735a328c52c2cd7fb9ac650f429b35cdf9943a
Ulf Sjödin <sjodin.ulf@gmail.com>
2025-04-18 20:17:30 +0200

    Har skapat koden för att mata in data om stationer.

--- src/db/operation.rs
+++ src/db/operation.rs
@@ -1,3 +1,9 @@
-pub fn hej() {
-    println!("Hej operation!")
+use rusqlite::{Connection, Result, params};
+
+pub fn insert_into_station(conn: &Connection, id: i32, name: &str, lat: f64, long: f64 ) -> Result<()> {
+    conn.execute(
+        "INSERT OR IGNORE INTO station (id, name, latitude, longitude) 
+        VALUES (?1, ?2, ?3)", params![id, name, lat, long],
+    )?;
+Ok(())
 }
