bf0325c972bfb46e03977497a4464bfc077bb843
Ulf Sjödin <sjodin.ulf@gmail.com>
2025-04-18 19:39:09 +0200

    har skapat schema för två tabeller(station och observations).

--- src/db/connection.rs
+++ src/db/connection.rs
@@ -1,3 +1,7 @@
-pub fn hej() {
-    println!("Hej connection!")
+use rusqlite::{params, Connection, Result};
+
+pub fn open_connection() -> Result<Connection> {
+    // Här anges databasens filnamn om den redan inte är skapad.
+    let conn = Connection::open("smhi.db")?;
+    Ok(conn)
 }
--- src/db/schema.rs
+++ src/db/schema.rs
@@ -1,3 +1,30 @@
-pub fn hej() {
-    println!("Hej Schema!")
+use rusqlite::{Connection, Result};
+
+pub fn create_station_table(conn: &Connection) -> Result<()> {
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS station (
+        id INTEGER PRIMARY KEY,
+        name TEXT NOT NULL,
+        latitude REAL NOT NULL,
+        longitude REAL NOT NULL
+    )",
+    [],
+)?;
+Ok(())
+}
+
+pub fn create_observation_table(conn: &Connection) -> Result<()> {
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS observations (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        station_id INTEGER NOT NULL,
+        timestamp TEXT NOT NULL,
+        parameter TEXT NOT NULL,
+        value REAL NOT NULL,
+        FOREIGN KEY(station_id) REFERENCES station(id),
+        UNIQUE (station_id, timestamp, parameter)
+    )",
+[],
+    )?;
+Ok(())
 }
